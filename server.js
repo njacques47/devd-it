c48371ee946a1891bb755dcd34ba9eabe90d38ea
njacques47 <nojacques94@gmail.com>
2022-03-30 23:11:59 -0400

    build sample database call

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
+db.query(`SELECT * FROM  candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World'
